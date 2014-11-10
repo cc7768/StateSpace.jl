ca777d0d8711fc54c633a884cd46f6ef5fa30b1b
chase <cc7768@gmail.com>
2014-11-10 11:33:19 -0500

    INDENTATION: Changed all indentation from tabs to spaces.

--- src/StateSpace.jl
+++ src/StateSpace.jl
@@ -4,16 +4,16 @@ import Base: mean, filter, show
 
 
 export
-	LinearGaussianSSM,
-	NonlinearGaussianSSM,
-	FilteredState,
-	show,
-	predict,
-	observe,
-	update,
-	update!,
-	filter,
-	simulate
+    LinearGaussianSSM,
+    NonlinearGaussianSSM,
+    FilteredState,
+    show,
+    predict,
+    observe,
+    update,
+    update!,
+    filter,
+    simulate
 
 include("common.jl")
 include("KalmanFilter.jl")
